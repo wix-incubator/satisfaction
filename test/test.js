da7750ed359aae2a7ce2c7a8e3225c7e22afcaf0
Dany Shaanan <danyshaanan@gmail.com>
2015-09-20 21:33:58 +0300

    Refactor tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var fs = require('fs')
 var test = require('tape')
 var path = require('path')
 var satisfaction = require('../main.js')
@@ -17,30 +18,11 @@ test('this package', function(t) {
   t.end()
 })
 
-var passingDirectories = {
-  passing1: 'passing for dep, devdep, and git url with semver-like tag',
-  passing2: 'passing for githubuser/githubrepo dep with semver-like tag'
-}
+var tests = fs.readdirSync('test').filter(function(f) { return ~f.indexOf('_') })
 
-Object.keys(passingDirectories).forEach(function(dir) {
-  test(passingDirectories[dir], function(t) {
-    t.ok(statusForDir(dir))
-    t.end()
-  })
-})
-
-var failingDirectories = {
-  failing1: 'failing on wrong version',
-  failing2: 'failing on missing dependency',
-  failing3: 'failing on missing dev dependency',
-  failing4: 'failing on git url with non semver-like tag',
-  failing5: 'failing on missing git url dependency',
-  failing6: 'failing on wrong version with githubuser/githubrepo dep'
-}
-
-Object.keys(failingDirectories).forEach(function(dir) {
-  test(failingDirectories[dir], function(t) {
-    t.notOk(statusForDir(dir))
+tests.forEach(function(dir) {
+  test(dir, function(t) {
+    t.equals(statusForDir(dir), dir.indexOf('passing') === 0)
     t.end()
   })
 })
