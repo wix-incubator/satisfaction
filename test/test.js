da777d13c1b2b8a4c6035345010d16ea1ed6f3ae
Dany Shaanan <danyshaanan@gmail.com>
2015-09-20 17:33:20 +0300

    Refactor main test file

--- test/test.js
+++ test/test.js
@@ -13,46 +13,34 @@ function statusForDir(dir) {
 }
 
 test('this package', function(t) {
-  t.plan(1)
-  t.equal(satisfaction.status(), true)
+  t.ok(satisfaction.status())
+  t.end()
 })
 
-test('passing for dep, devdep, and git url with semver-like tag', function(t) {
-  t.plan(1)
-  t.equal(statusForDir('passing1'), true)
+var passingDirectories = {
+  passing1: 'passing for dep, devdep, and git url with semver-like tag',
+  passing2: 'passing for githubuser/githubrepo dep with semver-like tag'
+}
+
+Object.keys(passingDirectories).forEach(function(dir) {
+  test(passingDirectories[dir], function(t) {
+    t.ok(statusForDir(dir))
+    t.end()
+  })
 })
 
-test('passing for githubuser/githubrepo dep with semver-like tag', function(t) {
-  t.plan(1)
-  t.equal(statusForDir('passing2'), true)
-})
+var failingDirectories = {
+  failing1: 'failing on wrong version',
+  failing2: 'failing on missing dependency',
+  failing3: 'failing on missing dev dependency',
+  failing4: 'failing on git url with non semver-like tag',
+  failing5: 'failing on missing git url dependency',
+  failing6: 'failing on wrong version with githubuser/githubrepo dep'
+}
 
-test('failing on wrong version', function(t) {
-  t.plan(1)
-  t.equal(statusForDir('failing1'), false)
-})
-
-test('failing on missing dependency', function(t) {
-  t.plan(1)
-  t.equal(statusForDir('failing2'), false)
-})
-
-test('failing on missing dev dependency', function(t) {
-  t.plan(1)
-  t.equal(statusForDir('failing3'), false)
-})
-
-test('failing on git url with non semver-like tag', function(t) {
-  t.plan(1)
-  t.equal(statusForDir('failing4'), false)
-})
-
-test('failing on missing git url dependency', function(t) {
-  t.plan(1)
-  t.equal(statusForDir('failing5'), false)
-})
-
-test('failing on missing git url dependency', function(t) {
-  t.plan(1)
-  t.equal(statusForDir('failing6'), false)
+Object.keys(failingDirectories).forEach(function(dir) {
+  test(failingDirectories[dir], function(t) {
+    t.notOk(statusForDir(dir))
+    t.end()
+  })
 })
